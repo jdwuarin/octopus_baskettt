60c8fdab54bd46d18abd33a588de016baddc050e
Arnaud Benard <arnaudbenard13@gmail.com>
2013-08-16 15:42:38 +0200

    Refactored the file system to have a better structure

---
+++ octopusapp/apps/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from mongoengine import *
+
+# Create your models here.
+class User(Document):
+    email = StringField(required=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)

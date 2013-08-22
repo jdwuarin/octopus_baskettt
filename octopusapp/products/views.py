4068da5629f4f195f2033e0aaa3bed481af6c88d
Arnaud Benard <arnaudbenard13@gmail.com>
2013-08-22 18:24:43 +0200

    added basic boilerplate html

--- octopusapp/products/views.py
+++ octopusapp/products/views.py
@@ -13,8 +13,10 @@ def show(request, product_id):
 	product = get_obj_or_404(Product, pk=product_id)
 	return render(request, 'products/show.html', {'product': product})
 
-def get_obj_or_404(klass, *args, **kwargs):
-	try:
+
+# Because we have a mongoengine model and not a Django, we have to redefine the function
+def get_obj_or_404(klass, *args, **kwargs): 
+	try:									
 		return klass.objects.get(*args, **kwargs)
 	except klass.DoesNotExist:
 		raise Http404

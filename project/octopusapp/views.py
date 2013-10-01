13561843c8b7b69791712a148d27559f79dd6d26
Arnaud Benard <arnaudbenard13@gmail.com>
2013-10-01 17:28:53 +0200

    cleaned up the code

--- project/octopusapp/views.py
+++ project/octopusapp/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render
-from django.http import Http404
-
-from products.models import Product
 
 def index(request):
-	latest_products = Product.objects
-	context = {'latest_products': latest_products}
-	return render(request, 'products/index.html', context)
+	return render(request, 'products/index.html')

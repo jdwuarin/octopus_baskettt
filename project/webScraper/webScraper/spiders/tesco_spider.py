03a2836747e2a5eb83d44a2f53acadbce9f478a3
john-dwuarin <john.dwuarin@gmail.com>
2013-11-08 00:07:41 +0000

    did first iteration of tesco scraper

--- project/webScraper/webScraper/spiders/tesco_spider.py
+++ project/webScraper/webScraper/spiders/tesco_spider.py
@@ -1,30 +1,57 @@
-from scrapy.spider import BaseSpider
+
 from scrapy.selector import HtmlXPathSelector
-from scrapy.http import Request
+#from scrapy.http import Request
 from scrapy import log
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 from webScraper.items import WebscraperItem
 
 
-class TescoSpider(BaseSpider):
+class TescoSpider(CrawlSpider):
     name = 'tesco'
     allowed_domains = ["tesco.com", "secure.tesco.com"]
+
     start_urls = [
-        "http://www.tesco.com/groceries/product/browse/default.aspx?N=4294793658&Ne=4294793660&lvl=3"
+        "http://www.tesco.com/groceries/"
     ]
 
-    def parse(self, response):
+    rules = (
 
-        #continue scraping with authenticated session...
+        #first level
+        Rule (SgmlLinkExtractor(allow=("/groceries/department", ), restrict_xpaths=('//ul[@class="navigation Groceries"]',))
+        , follow= True),
+
+        #second level
+        Rule (SgmlLinkExtractor(allow=("/groceries/product/browse", ), restrict_xpaths=('//div[@class="clearfix"]',))
+        , callback="parse_listing_page", follow= True),
+
+        #finally down to the parsing level
+        Rule (SgmlLinkExtractor(allow=("lvl=3", ), restrict_xpaths=('//p[@class="next"]',))
+        , callback="parse_listing_page", follow= True),
+
+    )
+
+
+    def parse_listing_page(self, response):
+
         hxs = HtmlXPathSelector(response)
+
         products = hxs.select('//div[contains(@class, "desc")]')
         names = products.select('//a[contains(@href, "/groceries/Product/Details/")]/text()').extract()
         prices = products.select('//span[contains(@class, "linePrice")]/text()').extract()
         links = products.select('//a[contains(@href, "/groceries/Product/Details/")]/@href').extract()
 
+
         items = []
 
         for i in range(len(products)):
+
+            if (products[i].extract().startswith('<div class="descContent')):
+                continue
+
+           # if products[i]
+            # if i == 0 or i == 1:
 
             item = WebscraperItem()
 

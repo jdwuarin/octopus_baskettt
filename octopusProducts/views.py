e1c58b4eaabe187a05e2a3177a1c3081c49e70c8
john-dwuarin <john.dwuarin@gmail.com>
2013-12-09 01:52:59 +0000

    before changing it up

--- octopusProducts/views.py
+++ octopusProducts/views.py
@@ -3,8 +3,16 @@ from scrapy import log
 
 from twisted.internet import reactor
 from basket_to_port import Basket_to_port
-from reactor_thread_controller import Reactor_thread_controller
+from webScraper.webScraper.spiders.tesco_basket_spider import TescoBasketSpider
+from scrapy.crawler import Crawler
+from scrapy import log
+from scrapy.utils.project import get_project_settings
+from scrapy import signals
+from django.shortcuts import render
+import json
+from django.http import HttpResponse
 
+from spider_manager import Spider_manager_controller
 
 import json
 
@@ -14,26 +22,21 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'products/index.html')
 
-
-
 def spider_view(request):
 
-    #start reactor_thread if not already started
-
-    Reactor_thread_controller.create_and_start_thread()
+    Spider_manager_controller.create_if_none()
 
     product_details = {"http://www.tesco.com/groceries/Product/Details/?id=268768585": "1"}
 
     basket = Basket_to_port(request, "arnaudbenard13+test@gmail.com", "test123", product_details)
 
-    Reactor_thread_controller.add_basket_to_port(basket)
-
+    Spider_manager_controller.add_basket_to_port(basket)
 
     response_data = {} 
     response_data['itemAddedToBasket'] = 'False'
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
+
 def start_reactor(request):
     if not reactor.running:
         reactor.run(installSignalHandlers=False)
-

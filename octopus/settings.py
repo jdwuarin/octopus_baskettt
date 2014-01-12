d3ce34aea9565e14c6bb4526933938902fe525b8
Arnaud Benard <arnaudbenard13@gmail.com>
2014-01-12 11:20:16 +0000

    Renamed the media folder to assets

--- octopus/settings.py
+++ octopus/settings.py
@@ -55,7 +55,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR,'../media/'),
+    os.path.join(PROJECT_DIR,'../assets/'),
     os.path.join(PROJECT_DIR,'../bower_components/'),
 
     #os.path.join(PROJECT_DIR,'../bin/'),

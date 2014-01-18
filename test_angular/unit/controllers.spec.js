54c0870360eeddd555e6a89ade74e3315f936293
Arnaud Benard <arnaudbenard13@gmail.com>
2014-01-18 18:02:49 +0000

    testing that the controller is getting data from localstorage

--- test_angular/unit/controllers.spec.js
+++ test_angular/unit/controllers.spec.js
@@ -1,1 +1,43 @@
-// JASMINE TEMPLATE
+describe('OnboardingController', function(){
+
+	//Mocks
+	var windowMock, httpBackend, _Preference, sharedMock;
+
+	//Controller
+	var ctrl;
+
+	//Scope
+	var ctrlScope;
+
+	//Data
+	var cuisines = [
+		{ "name": "Italian", "image": "italian.png"},
+		{ "name": "Chinese", "image": "chinese.png"},
+		{ "name": "Indian", "image": "indian.png"},
+		{ "name": "Spanish", "image": "spanish.png"},
+		{ "name": "Thai",  "image": "thai.png"},
+		{ "name": "French",  "image": "french.png"}
+	];
+
+	//Loading shopping module
+	beforeEach(function () {
+		module("App");
+	});
+
+	beforeEach(inject(function ($rootScope, $httpBackend, $controller, Preference) {
+
+		// New scope
+		ctrlScope = $rootScope.$new();
+		_Preference = Preference;
+
+		spyOn(Preference, 'getAll').andCallThrough();
+
+		//Creating controller with assigning mocks instead of actual services
+		ctrl = $controller('OnboardingController', { $scope: ctrlScope, Preference: _Preference});
+	}));
+
+
+	it("Should get the Preferences from a previous session", function(){
+		expect(_Preference.getAll()).not.toBe(null);
+	});
+});
--- test_angular/unit/services.spec.js
+++ test_angular/unit/services.spec.js
@@ -1,27 +1,27 @@
 
-describe('App services', function() {
+// describe('App services', function() {
 
-	beforeEach(function(){
-		module('ngMock');
-		module('App');
-	});
+// 	beforeEach(function(){
+// 		module('ngMock');
+// 		module('App');
+// 	});
 
-	describe('User', function(){
+// 	describe('User', function(){
 
-		it('should be able to login', inject(['User', function(User) {
-			// from django.contrib.auth.models import User
-			// User.objects.create_user('test', 'test@test.com', 'testpassword')
-			var validEmail		= "test";
-			var validPassword	= "testpassword";
-			expect(true).toBe(true);
-			// User.login(validEmail, validPassword, function(res){
+// 		it('should be able to login', inject(['User', function(User) {
+// 			// from django.contrib.auth.models import User
+// 			// User.objects.create_user('test', 'test@test.com', 'testpassword')
+// 			var validEmail		= "test";
+// 			var validPassword	= "testpassword";
+// 			expect(true).toBe(true);
+// 			// User.login(validEmail, validPassword, function(res){
 
-			// });
+// 			// });
 
 
-		}])
-		);
+// 		}])
+// 		);
 
-	});
+// 	});
 
-});
+// });

83c5a4fbd944ff8d742f9588fa60da603337be41
john-dwuarin <john.dwuarin@gmail.com>
2014-03-19 21:12:38 +0000

    wrote clean_user_settings routine

--- octopus_recommendation_engine/recommendations_automation.py
+++ octopus_recommendation_engine/recommendations_automation.py
@@ -1,5 +1,19 @@
+from octopus_user.models import *
+from datetime import datetime
+from datetime import timedelta
 
 def recommendation_automation():
 
     #go through all user_settings and only take the one that have an attached user
+    clean_user_settings()
+
     pass
+
+def clean_user_settings():
+    user_settings_list = UserSettings.objects.all()
+
+    three_days = timedelta(days=3)
+    for user_settings in user_settings_list:
+        if not user_settings.user and (
+                    datetime.now() - user_settings.time > three_days):
+            user_settings.delete()
